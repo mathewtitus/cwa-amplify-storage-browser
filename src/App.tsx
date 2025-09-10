660f249d210917e3748d0a097fe85e43f511667f
Mathew Titus <litsos.titus@gmail.com>
2025-09-10 16:48:04 -0700

    Fixing AuthUser call to nonexistent attribute

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
       {({ signOut, user }) => (
         <>
           <Flex direction="row" alignItems="center" wrap="nowrap" gap="1rem">
-            <Heading level={4}>{`Hello ${user?.name}`}</Heading>
+            <Heading level={4}>{`Hello ${user?.username}`}</Heading>
             <Button onClick={signOut}>Sign out</Button>
           </Flex>
           <StorageBrowser />
